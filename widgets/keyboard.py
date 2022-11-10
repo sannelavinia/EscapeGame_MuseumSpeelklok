feb93777f5beb21f47f42082e26e79b768ccd4af
Barmad_Shreef <barmad.studie98@gmail.com>
2022-11-10 01:25:38 +0100

    modified the size of the keyboard

--- widgets/keyboard.py
+++ widgets/keyboard.py
@@ -37,13 +37,13 @@ class Keyboard():
 
         # code frame
         self.code_frame_first_number = Text_frame(m.code_input_frame, m.button_width*3/4, m.button_height,
-                                                  "", m.white_color, m.code_font, self.x_pos-(m.button_width/8), self.y_pos - m.button_height)
+                                                  "", m.white_color, m.Quantico_font_70, self.x_pos-(m.button_width/8), self.y_pos - m.button_height)
         self.code_frame_second_number = Text_frame(m.code_input_frame, m.button_width*3/4, m.button_height,
-                                                   "", m.white_color, m.code_font, self.x_pos+5*(m.button_width/8), self.y_pos - m.button_height)
+                                                   "", m.white_color, m.Quantico_font_70, self.x_pos+5*(m.button_width/8), self.y_pos - m.button_height)
         self.code_frame_third_number = Text_frame(m.code_input_frame, m.button_width*3/4, m.button_height,
-                                                  "", m.white_color, m.code_font, self.x_pos+11 * (m.button_width/8), self.y_pos - m.button_height)
+                                                  "", m.white_color, m.Quantico_font_70, self.x_pos+11 * (m.button_width/8), self.y_pos - m.button_height)
         self.code_frame_fourth_number = Text_frame(m.code_input_frame, m.button_width*3/4, m.button_height,
-                                                   "", m.white_color, m.code_font, self.x_pos+17 * (m.button_width/8), self.y_pos - m.button_height)
+                                                   "", m.white_color, m.Quantico_font_70, self.x_pos+17 * (m.button_width/8), self.y_pos - m.button_height)
 
     #######################################################################################
     def display(self):
