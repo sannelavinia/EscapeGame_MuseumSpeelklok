995feff4aa254ecc51eb2caf7a189b354ebd8f25
Barmad_Shreef <barmad.studie98@gmail.com>
2022-11-10 01:28:45 +0100

    modified the instruction_box class to be able to change the position of the text

--- widgets/instruction_box.py
+++ widgets/instruction_box.py
@@ -9,11 +9,20 @@ class Instruction_Box():
     """
 
     #######################################################################################
-    def __init__(self, image, image_width, image_height, input_text, text_color, x_pos, y_pos, text_font=None ):
+    def __init__(self, image, image_width, image_height, input_text, text_color, x_pos, y_pos, text_font=None, lines_height=None, text_x_pos=None, text_y_pos=None):
         """initializer"""
 
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.text_x_pos = text_x_pos
+        if self.text_x_pos == None:
+            self.text_x_pos = x_pos + 45
+        self.text_y_pos = text_y_pos
+        if self.text_y_pos == None:
+            self.text_y_pos = y_pos + 50
+        self.lines_height = lines_height
+        if self.lines_height == None:
+            self.lines_height = m.line_space
         self.image = m.pygame.transform.scale(
             image, (image_width, image_height))
 
@@ -33,5 +42,6 @@ class Instruction_Box():
         y_pos = self.y_pos
         m.SCREEN.blit(self.image, (x_pos, y_pos))
         for element in self.text:
-            m.SCREEN.blit(element, (x_pos + 45, y_pos + 50))
-            y_pos += m.line_space
+            m.SCREEN.blit(
+                element, (x_pos + self.text_x_pos, y_pos + self.text_y_pos))
+            y_pos += self.lines_height
