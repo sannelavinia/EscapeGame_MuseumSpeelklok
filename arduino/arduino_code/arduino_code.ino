66c31bb017528e0ee59ea5f39ed96e83c2c7b4c6
aster <s2955199@vuw.leidenuniv.nl>
2022-12-05 17:03:14 +0100

    commentaar aangepast in arduino code

--- arduino/arduino_code/arduino_code.ino
+++ arduino/arduino_code/arduino_code.ino
@@ -1,7 +1,7 @@
 // 03-12-2022
 // Arduino code to receive information from python code 
 // to turn lamps and machine on and off
-// Riham Mushmush
+// Software Engineering Group 14, Leiden University 
 
 String inBytes;
 String gameNum;
@@ -35,50 +35,48 @@ void loop() {
     // activeMachine 0,1,2,3,4,5
     // begin with 2 first instead of 0 to decrease number of comparisons because of that the games uses activeMachine=2 too
     if (machineNum == "2"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=0
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders for activeMachine=0 
     }
     else if (machineNum == "1"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=1
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders add orders for activeMachine=1
     }
     else if (machineNum == "0"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=2
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders add orders for activeMachine=2
     }
     else if (machineNum == "3"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=3
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders add orders for activeMachine=3
     }
     else if (machineNum == "4"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=4
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders add orders for activeMachine=4
     }
     else if (machineNum == "5"){
-      digitalWrite(LED_BUILTIN, HIGH);  // add orders for activeMachine=5
+      digitalWrite(LED_BUILTIN, HIGH);  // TODO: remove this line and instead add orders add orders for activeMachine=5
     }
-    // delay(1000);                       // wait for a second
 
     // activeGame 0,1,2,3,4,5,6
     gameNum = inBytes.substring(27);
 
     if (gameNum == "0"){
-      digitalWrite(LED_BUILTIN, LOW);  // add orders for activeGame=0
+      digitalWrite(LED_BUILTIN, LOW);  // TODO: remove this line and instead add orders add orders for activeGame=0
     }
     else if (gameNum == "1"){
-      digitalWrite(A0, HIGH);  // add orders for activeGame=1
+      digitalWrite(A0, HIGH);  // orders for activeGame=1, lamp 1 is turned on 
     }
     else if (gameNum == "2"){
-      digitalWrite(A1, HIGH);  // add orders for activeGame=2
+      digitalWrite(A1, HIGH);  // orders for activeGame=2, lamp 2 is turned on 
     }
     else if (gameNum == "3"){
-      digitalWrite(A2, HIGH);  // add orders for activeGame=3
+      digitalWrite(A2, HIGH);  // orders for activeGame=3, lamp 3 is turned on 
     }
     else if (gameNum == "4"){
-      digitalWrite(A3, HIGH);  // add orders for activeGame=4
+      digitalWrite(A3, HIGH);  // orders for activeGame=4, lamp 4 is turned on 
     }
     else if (gameNum == "5"){
-      digitalWrite(A4, HIGH);  // add orders for activeGame=5
+      digitalWrite(A4, HIGH);  // orders for activeGame=5, lamp 5 is turned on 
     }
     else if (gameNum == "6"){
-      digitalWrite(A5, HIGH);  // add orders for activeGame=6
+      digitalWrite(A5, HIGH);  // orders for activeGame=6, lamp 6 is turned on 
     }
-    // delay(1000);                       // wait for a second
 
     Serial.write("stop");  // send a signal to python to stop the program when the signal is recived
   
