07cc7838b531a362770326c46c77cf8cdcc65621
Barmad_Shreef <barmad.studie98@gmail.com>
2022-12-05 22:46:30 +0100

    added some comments

--- constants.py
+++ constants.py
@@ -26,12 +26,22 @@ game_5_code = "8521"
 game_6_code = "1948"
 
 admin_code = "1111"
+
+# hold duration of the logo button (in milliseconds)
 restart_time_logo_pressed = 200
+
+# after finishing the game (in milliseconds)
 restart_time_end_window = 300000  # 5 min
 
 # normal times per game (in milliseconds)
-game_normal_time = 180000  # 3 min
+game_normal_time = 300000  # 5 min
+
+# times related to the tip button (in milliseconds)
 game_tip_1_time = 180000  # 3 min
 game_tip_2_time = 240000  # 4 min (3 min first tip + 1 min second tip)
+tip_1_penalty_time = 180000  # 3 min
+tip_2_penalty_time = 180000  # 3 min
+
+# animation times (in milliseconds)
 correct_code_animation_delay = 6000  # 6 seconds gears animation delay
 incorrect_code_animation_delay = 11000  # 11 seconds gears animation delay
